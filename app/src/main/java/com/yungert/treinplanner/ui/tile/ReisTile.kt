a52a75017ff023350a335991a936cea352b7ea23
Yungert <98689766+Yungert@users.noreply.github.com>
2023-09-11 20:45:59 +0200

    tile

--- app/src/main/java/com/yungert/treinplanner/ui/tile/ReisTile.kt
+++ app/src/main/java/com/yungert/treinplanner/ui/tile/ReisTile.kt
@@ -1,5 +1,9 @@
 package com.yungert.treinplanner.ui.tile
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.wear.tiles.DimensionBuilders
 import androidx.wear.tiles.LayoutElementBuilders
 import androidx.wear.tiles.RequestBuilders
@@ -7,6 +11,11 @@ import androidx.wear.tiles.ResourceBuilders
 import androidx.wear.tiles.TileBuilders
 import androidx.wear.tiles.TileService
 import androidx.wear.tiles.TimelineBuilders
+import com.yungert.treinplanner.presentation.Data.Repository.NsApiRepository
+import com.yungert.treinplanner.presentation.Data.Repository.SharedPreferencesRepository
+import com.yungert.treinplanner.presentation.Data.api.NSApiClient
+import com.yungert.treinplanner.presentation.Data.api.Resource
+import com.yungert.treinplanner.presentation.Data.models.Leg
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.guava.future
@@ -14,9 +23,12 @@ import kotlinx.coroutines.guava.future
 private const val RESOURCES_VERSION = "1"
 
 class ReisTile: TileService() {
-
+    private val nsApiRepository: NsApiRepository = NsApiRepository(NSApiClient)
     private val serviceScope = CoroutineScope(Dispatchers.IO)
+    private val sharedPreferencesRepository: SharedPreferencesRepository = SharedPreferencesRepository()
+
     override fun onTileRequest(requestParams: RequestBuilders.TileRequest) = serviceScope.future {
+
         val singleTileTimeline = TimelineBuilders.Timeline.Builder()
             .addTimelineEntry(
                 TimelineBuilders.TimelineEntry.Builder()
@@ -42,8 +54,17 @@ class ReisTile: TileService() {
     }
 
 
-    private fun tileLayout(): LayoutElementBuilders.LayoutElement {
-        val text = "test1234"
+    private suspend fun tileLayout(): LayoutElementBuilders.LayoutElement {
+        var data = "default waarde"
+
+        var adies = ""
+        val reis = getReisadvies()
+        reis.forEach { rit ->
+            adies+= rit.product.number
+        }
+        data = adies
+
+        val text = data
         return LayoutElementBuilders.Box.Builder()
             .setVerticalAlignment(LayoutElementBuilders.VERTICAL_ALIGN_CENTER)
             .setWidth(DimensionBuilders.expand())
@@ -54,5 +75,24 @@ class ReisTile: TileService() {
                     .build()
             )
             .build()
+    }
+
+    private suspend fun getReisadvies(): List<Leg>{
+        val id = sharedPreferencesRepository.getReisadviesId(context = applicationContext, "reisadviesId")
+        if (id == "" && id != null){
+            return emptyList()
+        }
+        var data = emptyList<Leg>()
+        nsApiRepository.fetchSingleTripById(reisadviesId = id!!).collect { result ->
+            when (result) {
+                is Resource.Success -> {
+                    data = result.data?.legs!!
+                }
+                else -> {
+
+                }
+            }
+        }
+        return data
     }
 }
